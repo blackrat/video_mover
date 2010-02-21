4c8f8783eaa02e34b13bac2b0b8c9f82b1f6f26d
paul <paul@703cad2d-3223-468a-a081-0a2ded0371ac>
2010-02-21 20:00:39 +0000

    rebuilt svn server

---
+++ BBCRadio/lib/BBCRadio.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+class BBCRadio
+    attr_reader :dir
+    def initialize(dir="~/.borg")
+        @dir=dir
+    end
+    def programme_name(date,time,channel)
+        if (date=="01/01/1970")
+            return "Unknown"
+        end
+        "Brian_Appletons_History_of_Rock_and_Roll"
+    end
+    def fetch(date,time,channel)
+        
+    end
+end
---
+++ BBCRadio/test/getstreams/BBCRobot.pm
@@ -0,0 +1,33 @@
+package BBCRobot;
+
+use strict;
+use warnings;
+
+use LWP::RobotUA;
+
+our $VERSION = sprintf "%d.%02d", '$Revision: 1.3 $ ' =~ /(\d+)\.(\d+)/;
+our @ISA = qw(LWP::RobotUA);
+
+my %defaults = (
+		agent => 'BBC Stream Crawler (http://dave.org.uk/streams/)',
+		delay => 3,
+		);
+
+sub new {
+  my $class = shift;
+
+  my $agent = shift || $defaults{agent};
+  my $from  = $ENV{BBC_ROBOT_FROM}
+    || die 'You must set the environment variable BBC_ROBOT_FROM ' .
+      "to be your email address\n";
+  my $rules = shift;
+  my $delay = $ENV{BBC_ROBOT_DELAY} || $defaults{delay};
+
+  my $ua = $class->SUPER::new($agent, $from, $rules);
+  warn "Robot delay is $delay minute(s)\n";
+  $ua->delay($delay);
+
+  return $ua;
+}
+
+1;
