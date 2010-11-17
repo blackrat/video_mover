98323fec7148ef25b72ab7e5fa930e2a09fab2a2
paul <paul@703cad2d-3223-468a-a081-0a2ded0371ac>
2010-11-17 15:16:35 +0000

    change to move file by file

--- MLDonkeyMove/archive_rebuild.rb
+++ MLDonkeyMove/archive_rebuild.rb
@@ -17,25 +17,27 @@ class ArchiveVideos
 
     def span_seasons(programme)
       seasons=Dir.glob(File.join(programme,'Season*')).collect {|y| y}
-      if seasons.size>1
-        new_seasons=[]
-        seasons.each do |x|
-          case x
-          when /\/(\d\d\d\d)\/.*\/Season(\d\d)/
-            year=$1.to_i
-            season=$2.to_i
-            year+=season+1 unless season==0
+      new_seasons=[]
+      seasons.each do |x|
+        case x
+        when /\/(\d\d\d\d)\/.*\/Season(\d\d)/
+          year=$1.to_i
+          season=$2.to_i
+          year+=season+1 unless season==0
+          if year < 2011
             new_seasons << x.gsub(/\/(\d\d\d\d)\//,"/#{year}/")
+          else
+            new_seasons << x
           end
         end
-        new_seasons.size.times do |x|
-          next if seasons[x]==new_seasons[x]
-          puts("Moving from #{seasons[x]} to #{new_seasons[x]}")
-          FileUtils.mkdir_p(new_seasons[x])
-          Dir.glob(File.join(seasons[x],'*')).each do |file|
-            puts("Moving #{file}")
-            FileUtils.mv(file,new_seasons[x])
-          end
+      end
+      new_seasons.size.times do |x|
+        next if seasons[x]==new_seasons[x]
+        puts("Moving from #{seasons[x]} to #{new_seasons[x]}")
+        FileUtils.mkdir_p(new_seasons[x])
+        Dir.glob(File.join(seasons[x],'*')).each do |file|
+          puts("Moving #{file}")
+          FileUtils.mv(file,new_seasons[x])
         end
       end
     end
