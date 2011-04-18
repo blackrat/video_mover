aaa085fdebbf46bc4774e55833f3af66daa097ba
paul <paul@703cad2d-3223-468a-a081-0a2ded0371ac>
2011-04-18 12:55:41 +0000

    moved old experimental branch

--- BBCRadio/lib/BBCBackStage.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-BBC_BACKSTAGE='http://backstage.bbc.co.uk/feeds/tvradio/#{date}.tar.gz'
--- BBCRadio/lib/test.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require "minitar"
-
-uri='http://www.google.com'
-open(uri)
